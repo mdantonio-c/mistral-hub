5ec5e6fa98a9632904a1437bb92c1776687670d9
Mattia <m.dantonio81@gmail.com>
2019-06-11 09:38:54 +0200

    Compatibility fix

---
+++ projects/mistral/frontend/app/custom.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { RapydoModule } from '/rapydo/src/app/rapydo.module';
+import { AuthGuard } from '/rapydo/src/app/app.auth.guard';
+
+import { DataComponent } from './components/data'
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/app/data',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app',
+    redirectTo: '/app/data',
+    pathMatch: 'full'
+  },
+  {
+    path: 'app/data',
+    component: DataComponent,
+    canActivate: [AuthGuard],
+    runGuardsAndResolvers: 'always',
+  },
+];
+
+@NgModule({
+	imports: [
+		RapydoModule,
+	    RouterModule.forChild(routes),
+	],
+	declarations: [
+		DataComponent
+	],
+
+	providers: [
+	],
+
+	exports: [
+		RouterModule
+  	]
+
+})
+export class CustomModule {
+} 
