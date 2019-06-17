0acbe6f78fdc743de81d19d3bea1193393c5aade
Giuseppe Trotta <g.trotta@cineca.it>
2019-06-17 17:08:10 +0200

    simple implementation of data extraction with user quota check and result file saving

--- projects/mistral/backend/apis/data.py
+++ projects/mistral/backend/apis/data.py
@@ -30,10 +30,22 @@ class Data(EndpointResource):
     @catch_error()
     def post(self):
 
-        # ########## INIT ##########
+        user = self.get_current_user()
+        logger.info('request for data extraction coming from user UUID: {}'.format(user.uuid))
+        input_parameters = self.get_input()
+
+        datasets = input_parameters.get('datasets', [])
+        if not datasets:
+            raise RestApiException(
+                "Please specify at least one dataset",
+                status_code=hcodes.HTTP_BAD_REQUEST)
+        # TODO check for existing dataset(s)
+
+        filters = input_parameters.get('filters')
+        # TODO check for valid and allowed filters
 
         task = CeleryExt.data_extract.apply_async(
-            args=[],
+            args=[user.uuid, datasets, filters],
             countdown=1
         )
         return self.force_response(
