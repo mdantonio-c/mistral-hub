42c68d77083db5b0d15574c8dfdfcd0fe2b1e48d
bchiavarini <b.chiavarini@cineca.it>
2019-07-26 12:17:08 +0200

    request API for retrieve request records given a uuid

---
+++ projects/mistral/backend/apis/requests.py
@@ -0,0 +1,24 @@
+from restapi.rest.definition import EndpointResource
+from restapi.exceptions import RestApiException
+from restapi.decorators import catch_error
+from utilities import htmlcodes as hcodes
+from utilities.logs import get_logger
+from mistral.services.requests_manager import RequestManager
+
+import datetime
+
+log = get_logger(__name__)
+
+
+class UserRequests(EndpointResource):
+    @catch_error()
+    def get(self):
+        param= self.get_input()
+        uuid = param.get('uuid')
+        #log.info('parameters: {}'.format(uuid))
+
+        db = self.get_service_instance('sqlalchemy')
+        request_list = RequestManager.get_user_requests(db,uuid)
+
+        return self.force_response(
+            request_list, code=hcodes.HTTP_OK_BASIC)
--- projects/mistral/backend/services/requests_manager.py
+++ projects/mistral/backend/services/requests_manager.py
@@ -15,3 +15,24 @@ class RequestManager ():
         request_id = r.id
 
         return request_id
+
+    @staticmethod
+    def get_user_requests (db,uuid):
+        request = db.Request
+        query_list = request.query.filter(request.user_uuid == uuid)
+
+        user = db.User
+        current_user = user.query.filter(user.uuid == uuid).first()
+        user_name = current_user.name
+
+        request_list = []
+        for row in query_list:
+            user_request = {}
+            user_request['submission_date'] = row.submission_date
+            user_request['args'] = json.loads(row.args)
+            user_request['user_name'] = user_name
+            user_request['status'] = row.status
+            user_request['fileoutput'] = "file.file"
+            request_list.append(user_request)
+
+        return request_list
