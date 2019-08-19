a4b88dbaaf1c2758c8a41b913b1b0cc5b1bbbbe1
Giuseppe Trotta <g.trotta@cineca.it>
2019-08-19 10:30:58 +0200

    load user requests with ngx-datatable

--- projects/mistral/backend/apis/requests.py
+++ projects/mistral/backend/apis/requests.py
@@ -3,7 +3,7 @@ from restapi.exceptions import RestApiException
 from restapi.decorators import catch_error
 from utilities import htmlcodes as hcodes
 from utilities.logs import get_logger
-from mistral.services.requests_manager import RequestManager
+from mistral.services.requests_manager import RequestManager as repo
 
 import datetime
 
@@ -11,26 +11,32 @@ log = get_logger(__name__)
 
 
 class UserRequests(EndpointResource):
+
     @catch_error()
     def get(self):
-        param= self.get_input()
-        uuid = param.get('uuid')
+        param = self.get_input()
+        # uuid = param.get('uuid')
         sort = param.get('sort-by')
         sort_order = param.get('sort-order')
         filter = param.get('filter')
+        get_total = param.get('get_total', False)
+
         user = self.get_current_user()
-        log.info('current user:{}, requested user: {}'.format(user.uuid,uuid))
-        if user.uuid==uuid:
-            #log.info('parameters: {}'.format(uuid))
+        # log.info('current user:{}, requested user: {}'.format(user.uuid, uuid))
+        # if user.uuid != uuid:
+        #     raise RestApiException(
+        #         "Operation not allowed",
+        #         status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
 
-            db = self.get_service_instance('sqlalchemy')
+        db = self.get_service_instance('sqlalchemy')
 
-            # get user requests list
-            request_list = RequestManager.get_user_requests(db,uuid,sort_by=sort,sort_order= sort_order,filter=filter)
+        if get_total:
+            counter = repo.count_user_requests(db, user.uuid)
+            return {"total": counter}
 
-            return self.force_response(
-                request_list, code=hcodes.HTTP_OK_BASIC)
-        else:
-            raise RestApiException(
-                "Operation not allowed",
-                status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
+        # get user requests list
+        res = repo.get_user_requests(db, user.uuid, sort_by=sort, sort_order=sort_order,
+                                                    filter=filter)
+        return self.force_response(
+            res, code=hcodes.HTTP_OK_BASIC)
+
--- projects/mistral/frontend/app/components/requests/requests.component.ts
+++ projects/mistral/frontend/app/components/requests/requests.component.ts
@@ -1,14 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild, TemplateRef} from '@angular/core';
+import {BasePaginationComponent} from '/rapydo/src/app/components/base.pagination.component';
+
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ApiService} from '/rapydo/src/app/services/api';
+import {AuthService} from '/rapydo/src/app/services/auth';
+import {NotificationService} from '/rapydo/src/app/services/notification';
+import {FormlyService} from '/rapydo/src/app/services/formly'
 
 @Component({
-  selector: 'app-requests',
-  templateUrl: './requests.component.html'
+    selector: 'app-requests',
+    templateUrl: './requests.component.html'
 })
-export class RequestsComponent implements OnInit {
+export class RequestsComponent extends BasePaginationComponent {
 
-  constructor() { }
+    @ViewChild('dataSize', { static: false }) public dataSize: TemplateRef<any>;
+	@ViewChild('submissionDate', { static: false }) public submissionDate: TemplateRef<any>;
+	@ViewChild('dataStatus', { static: false }) public dataStatus: TemplateRef<any>;
+	@ViewChild('controlsCell', { static: false }) public controlsCell: TemplateRef<any>;
+	@ViewChild('emptyHeader', { static: false }) public emptyHeader: TemplateRef<any>;
 
-  ngOnInit() {
-  }
+    constructor(
+        protected api: ApiService,
+        protected auth: AuthService,
+        protected notify: NotificationService,
+        protected modalService: NgbModal,
+        protected formly: FormlyService,
+    ) {
+        super(api, auth, notify, modalService, formly);
+        this.init("group");
+
+		this.server_side_pagination = true;
+		this.endpoint = 'requests';
+		this.counter_endpoint = 'requests';
+		this.initPaging(20);
+		this.list();
+    }
+
+    list() {
+        let user = this.auth.getUser();
+        let params = {'uuid': user.uuid};
+		return this.get(this.endpoint, params);
+	}
+
+	download(filename) {
+      // TODO
+      console.log(`download ${filename}`);
+    }
+
+    ngAfterViewInit(): void {
+		this.columns = [
+	        {name: 'Product', prop: "name", flexGrow: 2},
+	        {name: 'Submission date', prop: "submission_date", flexGrow: 1, cellTemplate: this.submissionDate},
+	        {name: 'Size', prop: "size", flexGrow: 0.5, cellTemplate: this.dataSize},
+	        {name: 'Status', prop: "status", flexGrow: 0.5, cellTemplate: this.dataStatus},
+			{name: 'controls', prop: 'controls', cellTemplate: this.controlsCell, headerTemplate: this.emptyHeader, flexGrow: 0.2},
+		];
+	}
 
 }
