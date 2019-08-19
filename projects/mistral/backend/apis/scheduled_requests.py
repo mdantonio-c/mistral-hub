34c1e1b9c6509a26cc00186a99b826ebb71c7cb1
bchiavarini <b.chiavarini@cineca.it>
2019-08-19 11:56:33 +0200

    swagger definition for scheduled_requests endpoint

---
+++ projects/mistral/backend/apis/scheduled_requests.py
@@ -0,0 +1,38 @@
+from restapi.rest.definition import EndpointResource
+from restapi.exceptions import RestApiException
+from restapi.decorators import catch_error
+from utilities import htmlcodes as hcodes
+from utilities.logs import get_logger
+from mistral.services.requests_manager import RequestManager
+
+import datetime
+
+log = get_logger(__name__)
+
+
+class ScheduledRequests(EndpointResource):
+    @catch_error()
+    def get(self):
+        # param= self.get_input()
+        # uuid = param.get('uuid')
+        # sort = param.get('sort-by')
+        # sort_order = param.get('sort-order')
+        # filter = param.get('filter')
+        # user = self.get_current_user()
+        # log.info('current user:{}, requested user: {}'.format(user.uuid,uuid))
+        # if user.uuid==uuid:
+        #     #log.info('parameters: {}'.format(uuid))
+        #
+        #     db = self.get_service_instance('sqlalchemy')
+        #
+        #     # get user requests list
+        #     request_list = RequestManager.get_user_requests(db,uuid,sort_by=sort,sort_order= sort_order,filter=filter)
+        #
+        #     return self.force_response(
+        #         request_list, code=hcodes.HTTP_OK_BASIC)
+        # else:
+        #     raise RestApiException(
+        #         "Operation not allowed",
+        #         status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
+        return self.force_response(
+                "request_list", code=hcodes.HTTP_OK_BASIC)
