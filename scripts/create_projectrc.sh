333cc28213037b673f5deed14ce2af40b9bd91be
Mattia <m.dantonio81@gmail.com>
2019-09-13 10:51:56 +0200

    Added create projectrc script

---
+++ scripts/create_projectrc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+echo "project_configuration:" > .projectrc
+echo "  variables:" >> .projectrc
+echo "    env:" >> .projectrc
+echo "      CELERYUI_PASSWORD: test${RANDOM}" >> .projectrc
+echo "      CELERYUI_USER: test${RANDOM}' >> .projectrc
+
+
