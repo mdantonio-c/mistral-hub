9f8bb1216b2bf5cb4ecedc8571a7019ef31d884c
Mattia <m.dantonio81@gmail.com>
2019-09-13 10:52:39 +0200

    Bug fix

--- scripts/create_projectrc.sh
+++ scripts/create_projectrc.sh
@@ -5,6 +5,6 @@ echo "project_configuration:" > .projectrc
 echo "  variables:" >> .projectrc
 echo "    env:" >> .projectrc
 echo "      CELERYUI_PASSWORD: test${RANDOM}" >> .projectrc
-echo "      CELERYUI_USER: test${RANDOM}' >> .projectrc
+echo "      CELERYUI_USER: test${RANDOM}" >> .projectrc
 
 
