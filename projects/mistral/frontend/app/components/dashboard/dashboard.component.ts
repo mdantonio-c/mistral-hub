c2952d9785b8c6305d954fcf9a20f26ac199bd3c
Giuseppe Trotta <g.trotta@cineca.it>
2019-09-17 16:25:32 +0200

    restore the refresh button

--- projects/mistral/frontend/app/components/dashboard/dashboard.component.ts
+++ projects/mistral/frontend/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {RequestsComponent} from "../requests/requests.component";
+import {SchedulesComponent} from "../schedules/schedules.component";
 
 @Component({
     selector: 'app-dashboard',
@@ -7,9 +9,19 @@ import {Component} from '@angular/core';
 })
 export class DashboardComponent {
     selectedTabId = 'requests';
+    @ViewChild('rTab', {static: false}) requests: RequestsComponent;
+    @ViewChild('sTab', {static: false}) schedules: SchedulesComponent;
 
     onTabChange($event) {
         this.selectedTabId = $event.nextId;
+    }
+
+    list() {
+        if (this.selectedTabId == 'requests') {
+            this.requests.list();
+        } else {
+            this.schedules.list();
+        }
     }
 
 }
