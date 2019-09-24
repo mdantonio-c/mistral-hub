48a8e0547912445635206aceea1e29d763347cae
Mattia <m.dantonio81@gmail.com>
2019-09-24 19:02:32 +0200

    Testing core Downloader

--- projects/mistral/backend/apis/file.py
+++ projects/mistral/backend/apis/file.py
@@ -1,21 +1,25 @@
+
+import os
+from mimetypes import MimeTypes
+
 from restapi.rest.definition import EndpointResource
+from restapi.services.download import Downloader
 from restapi.exceptions import RestApiException
 from restapi.decorators import catch_error
 from utilities import htmlcodes as hcodes
 from utilities.logs import get_logger
 from mistral.services.requests_manager import RequestManager
 from flask import send_from_directory
-import os
 
 
 log = get_logger(__name__)
 DOWNLOAD_DIR = '/data'
 
 
-class FileDownload(EndpointResource):
+class FileDownload(EndpointResource, Downloader):
 
     @catch_error()
-    def get(self,filename):
+    def get(self, filename):
 
         user = self.get_current_user()
         db = self.get_service_instance('sqlalchemy')
@@ -24,7 +28,12 @@ class FileDownload(EndpointResource):
             user_dir = os.path.join(DOWNLOAD_DIR, user.uuid)
             log.info('directory: {}'.format(user_dir))
             # download the file as a response attachment
-            return send_from_directory(user_dir, filename, as_attachment=True)
+            # return send_from_directory(user_dir, filename, as_attachment=True)
+
+            filepath = os.path.join(user_dir, filename)
+            mime = MimeTypes()
+            mime_type = mime.guess_type(filepath)
+            return self.send_file_streamed(filepath, mime_type[0])
 
         else:
             raise RestApiException(
