db7cd0e1133cfb4e09167bc9707e13e182f80a8d
Giuseppe Trotta <g.trotta@cineca.it>
2019-11-13 14:55:49 +0100

    format apis code for OpenAPI specs

--- projects/mistral/backend/apis/requests.py
+++ projects/mistral/backend/apis/requests.py
@@ -17,8 +17,67 @@ class UserRequests(EndpointResource):
 
     # schema_expose = True
     labels = ['requests']
-    GET = {'/requests': {'summary': 'Get requests filtered by uuid.', 'parameters': [{'name': 'perpage', 'in': 'query', 'description': 'Number of videos returned', 'type': 'integer'}, {'name': 'currentpage', 'in': 'query', 'description': 'Page number', 'type': 'integer'}, {'name': 'sort-order', 'in': 'query', 'description': 'sort order', 'type': 'string', 'enum': ['asc', 'desc']}, {'name': 'sort-by', 'in': 'query', 'description': 'params to sort requests', 'type': 'string'}, {'name': 'get_total', 'in': 'query', 'description': 'Retrieve total number of requests', 'type': 'boolean', 'default': False}], 'responses': {'200': {'description': 'List of requests of an user', 'schema': {'$ref': '#/definitions/Requests'}}, '404': {'description': 'User has no requests'}, '401': {'description': 'Current user is not allowed to see requests of the user in query'}}}}
-    DELETE = {'/requests/<request_id>': {'summary': 'Delete a request', 'responses': {'200': {'description': 'Request deleted successfully.'}, '404': {'description': 'Request does not exist.'}, '401': {'description': 'The user is not authorized to delete this request.'}}}}
+    GET = {
+        '/requests': {
+            'summary': 'Get requests filtered by uuid.',
+            'parameters': [
+                {
+                    'name': 'perpage',
+                    'in': 'query',
+                    'description': 'Number of videos returned',
+                    'type': 'integer',
+                },
+                {
+                    'name': 'currentpage',
+                    'in': 'query',
+                    'description': 'Page number',
+                    'type': 'integer',
+                },
+                {
+                    'name': 'sort-order',
+                    'in': 'query',
+                    'description': 'sort order',
+                    'type': 'string',
+                    'enum': ['asc', 'desc'],
+                },
+                {
+                    'name': 'sort-by',
+                    'in': 'query',
+                    'description': 'params to sort requests',
+                    'type': 'string',
+                },
+                {
+                    'name': 'get_total',
+                    'in': 'query',
+                    'description': 'Retrieve total number of requests',
+                    'type': 'boolean',
+                    'default': False,
+                },
+            ],
+            'responses': {
+                '200': {
+                    'description': 'List of requests of an user',
+                    'schema': {'$ref': '#/definitions/Requests'},
+                },
+                '404': {'description': 'User has no requests'},
+                '401': {
+                    'description': 'Current user is not allowed to see requests of the user in query'
+                },
+            },
+        }
+    }
+    DELETE = {
+        '/requests/<request_id>': {
+            'summary': 'Delete a request',
+            'responses': {
+                '200': {'description': 'Request deleted successfully.'},
+                '404': {'description': 'Request does not exist.'},
+                '401': {
+                    'description': 'The user is not authorized to delete this request.'
+                },
+            },
+        }
+    }
 
     @catch_error()
     @authentication.required()
@@ -42,10 +101,13 @@ class UserRequests(EndpointResource):
         # get user requests list
         # res = repo.get_user_requests(db, user.id, sort_by=sort, sort_order=sort_order)
         data = []
-        requests = db.Request.query.filter_by(user_id=user.id) \
-            .options(joinedload(db.Request.fileoutput)) \
-            .order_by(db.Request.submission_date.desc()) \
-            .paginate(page, limit, False).items
+        requests = (
+            db.Request.query.filter_by(user_id=user.id)
+            .options(joinedload(db.Request.fileoutput))
+            .order_by(db.Request.submission_date.desc())
+            .paginate(page, limit, False)
+            .items
+        )
         log.debug(requests)
         for r in requests:
             item = {
@@ -54,7 +116,7 @@ class UserRequests(EndpointResource):
                 'args': json.loads(r.args),
                 'submission_date': r.submission_date.isoformat(),
                 'status': r.status,
-                'task_id': r.task_id
+                'task_id': r.task_id,
             }
             if r.schedule_id is not None:
                 item['schedule_id'] = r.schedule_id
@@ -68,8 +130,7 @@ class UserRequests(EndpointResource):
                 item['filesize'] = r.fileoutput.size
             data.append(item)
 
-        return self.force_response(
-            data, code=hcodes.HTTP_OK_BASIC)
+        return self.force_response(data, code=hcodes.HTTP_OK_BASIC)
 
     @catch_error()
     @authentication.required()
@@ -82,8 +143,8 @@ class UserRequests(EndpointResource):
         # check if the request exists
         if not repo.check_request(db, request_id=request_id):
             raise RestApiException(
-                "The request doesn't exist",
-                status_code=hcodes.HTTP_BAD_NOTFOUND)
+                "The request doesn't exist", status_code=hcodes.HTTP_BAD_NOTFOUND
+            )
 
         # check if the current user is the owner of the request
         if repo.check_owner(db, user.id, request_id=request_id):
@@ -95,4 +156,5 @@ class UserRequests(EndpointResource):
         else:
             raise RestApiException(
                 "This request doesn't come from the request's owner",
-                status_code=hcodes.HTTP_BAD_UNAUTHORIZED)
+                status_code=hcodes.HTTP_BAD_UNAUTHORIZED,
+            )
