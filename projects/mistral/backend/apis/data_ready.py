f949d1a3fb6298aac645c652a983a7e68fb11336
Mattia <m.dantonio81@gmail.com>
2019-12-13 14:09:34 +0100

    Experiments with on data ready schedules

--- projects/mistral/backend/apis/data_ready.py
+++ projects/mistral/backend/apis/data_ready.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from mistral.services.requests_manager import RequestManager
 from restapi.rest.definition import EndpointResource
 from restapi import decorators as decorate
 from restapi.protocols.bearer import authentication
@@ -26,5 +27,21 @@ class DataReady(EndpointResource):
         log.info("Cluster = %s", data.get("Cluster"))
         log.info("Model = %s", data.get("Model"))
         log.info("rundate = %s", data.get("rundate"))
+
+        db = self.get_service_instance('sqlalchemy')
+        # Create as a method in RequestManager -> get_all_schedule_requests
+        # scheduled_requests = []
+        schedules_list = db.Schedule.query.all()
+        for row in schedules_list:
+            r = RequestManager._get_schedule_response(row)
+            # scheduled_requests.append(r)
+
+            # se tra gli args manca run significa che chiede sia 00 sia 12
+            # reftime == 00 || reftime == 12,
+            # quindi modificare reftime ricevuto per impostare la corsa successiva
+            # == rundate at 00 || rundate at 12
+            log.critical(r)
+
+        # log.critical(scheduled_requests)
 
         return self.force_response("1", code=hcodes.HTTP_OK_ACCEPTED)
