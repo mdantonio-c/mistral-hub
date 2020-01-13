bec31de31e8dab9c6be15e0643b2a37c3399917b
Mattia <m.dantonio81@gmail.com>
2020-01-13 06:59:52 +0100

    Fixed tests, added missing env variables

--- scripts/create_projectrc.sh
+++ scripts/create_projectrc.sh
@@ -6,5 +6,7 @@ echo "  variables:" >> .projectrc
 echo "    env:" >> .projectrc
 echo "      CELERYUI_PASSWORD: test${RANDOM}" >> .projectrc
 echo "      CELERYUI_USER: test${RANDOM}" >> .projectrc
+echo "      RABBITMQ_USER: test${RANDOM}" >> .projectrc
+echo "      RABBITMQ_PASSWORD: test${RANDOM}" >> .projectrc
 
 
