1bae4e1a7a7a017da7305649346c86bb7f6327db
afinazzi <andrea.finazzi@dedagroup.it>
2020-02-05 18:44:53 +0100

    Prima bozza postprocessing di tipo 3 - Area crop

--- projects/mistral/frontend/app/components/multi-step-wizard/step-postprocess/step-postprocess.component.ts
+++ projects/mistral/frontend/app/components/multi-step-wizard/step-postprocess/step-postprocess.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {FormBuilder, FormGroup, FormArray} from '@angular/forms';
-import {FormDataService} from "@app/services/formData.service";
-import {DataService} from "@app/services/data.service";
-import {NotificationService} from '@rapydo/services/notification';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { FormDataService } from "@app/services/formData.service";
+import { DataService } from "@app/services/data.service";
+import { NotificationService } from '@rapydo/services/notification';
 
 @Component({
     selector: 'step-postprocess',
@@ -13,15 +13,51 @@ export class StepPostprocessComponent implements OnInit {
     title = 'Choose a post-processing';
     form: FormGroup;
     vars = [];
+    space_crop_boundings = [
+        {
+            code: 'ilon',
+            desc: 'Initial Lon',
+            validators: [
+                Validators.min(-180),
+                Validators.max(180)
+            ]
+        },
+        {
+            code: 'ilat',
+            desc: 'Initial Lat',
+            validators: [
+                Validators.min(-90),
+                Validators.max(90)
+            ]
+        },
+        {
+            code: 'flon',
+            desc: 'Final Lon',
+            validators: [
+                Validators.min(-180),
+                Validators.max(180)
+            ]
+        },
+        {
+            code: 'flat',
+            desc: 'Final Lat',
+            validators: [
+                Validators.min(-90),
+                Validators.max(90)
+            ]
+        },
+    ]
 
     constructor(private formBuilder: FormBuilder,
-                private router: Router,
-                private route: ActivatedRoute,
-                private formDataService: FormDataService,
-                private dataService: DataService,
-                private notify: NotificationService) {
+        private router: Router,
+        private route: ActivatedRoute,
+        private formDataService: FormDataService,
+        private dataService: DataService,
+        private notify: NotificationService) {
         this.form = this.formBuilder.group({
-            derived_variables: new FormArray([])
+            derived_variables: new FormArray([]),
+            space_type: ['crop'],
+            space_crop: new FormArray([])
         });
     }
 
@@ -37,6 +73,13 @@ export class StepPostprocessComponent implements OnInit {
         });
     }
 
+    private buildSpaceCrop() {
+        this.space_crop_boundings.map(bound => {
+            const control = this.formBuilder.control(0, bound.validators);
+            (this.form.controls.space_crop as FormArray).push(control);
+        })
+    }
+
     ngOnInit() {
         window.scroll(0, 0);
         this.dataService.getDerivedVariables().subscribe(
@@ -48,6 +91,7 @@ export class StepPostprocessComponent implements OnInit {
                 this.notify.showError('Unable to load derived variables configuration');
             }
         )
+        this.buildSpaceCrop();
     }
 
     private save() {
@@ -65,21 +109,57 @@ export class StepPostprocessComponent implements OnInit {
                 variables: selectedDerivedVariables
             });
         }
+        // push space processor object in selectedProcessors
+        // TODO: space pp type <==> radio buttons binding
+        const selectedSpaceProcessor = this.form.value.space_type;
+        if (selectedSpaceProcessor && selectedSpaceProcessor.length) {
+            switch (selectedSpaceProcessor) {
+                case 'crop': {
+                    selectedProcessors.push(this.calculateSpaceCrop());
+                    break;
+                }
+
+                case 'grid': {
+                    // selectedProcessors.push(this.calculateSpaceGrid());
+                    break;
+                }
+
+                case 'points': {
+                    // selectedProcessors.push(this.calculateSpacePoints());
+                    break;
+                }
+            }
+
+        }
+
         this.formDataService.setPostProcessor(selectedProcessors);
         return true;
+    }
+
+
+    calculateSpaceCrop() {
+        const boundings = {}
+        this.form.value.space_crop.map((value, i) => {
+            boundings[this.space_crop_boundings[i].code] = value;
+        })
+        return {
+            type: 'grid_cropping',
+            sub_type: 'coord',
+            boundings: boundings,
+        }
     }
 
     goToPrevious() {
         // Navigate to the dataset page
         this.router.navigate(
-            ['../', 'filters'], {relativeTo: this.route});
+            ['../', 'filters'], { relativeTo: this.route });
     }
 
     goToNext() {
         if (this.save()) {
             // Navigate to the postprocess page
             this.router.navigate(
-                ['../', 'submit'], {relativeTo: this.route});
+                ['../', 'submit'], { relativeTo: this.route });
         }
     }
 
