3ab1cbaf6af767c88b9cac68d70f99f6d3650ba3
Mattia <m.dantonio81@gmail.com>
2020-04-06 16:10:05 +0200

    Fixed tests on step filters

--- projects/mistral/frontend/app/components/multi-step-wizard/step-filters/step-filters.component.spec.ts
+++ projects/mistral/frontend/app/components/multi-step-wizard/step-filters/step-filters.component.spec.ts
@@ -1,9 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import {Router} from '@angular/router';
 import {ReactiveFormsModule, FormBuilder} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {DatePipe} from '@angular/common';
+import {NgxSpinnerModule} from "ngx-spinner";
 
 import {NotificationService} from '@rapydo/services/notification';
 import {StepFiltersComponent} from './step-filters.component';
@@ -28,8 +30,10 @@ describe('StepFiltersComponent', () => {
             imports: [
                 ReactiveFormsModule,
                 RouterTestingModule.withRoutes([]),
+                NgxSpinnerModule,
                 NgbModule
             ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
             providers: [
                 DatePipe,
                 {provide: FormBuilder, useValue: formBuilder},
