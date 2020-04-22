2235b2127b7be0dfa4af25321b2fc0153f454851
Mattia <m.dantonio81@gmail.com>
2020-04-22 11:50:51 +0200

    Custom fields in profile

--- projects/mistral/backend/apis/profile.py
+++ projects/mistral/backend/apis/profile.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
+from marshmallow import fields
 
 
 class CustomProfile(object):
     def __init__(self):
         pass
 
-    def manipulate(self, ref, user, data):
+    @staticmethod
+    def manipulate(ref, user, data):
         data['Disk Quota'] = user.disk_quota
 
         return data
+
+    @staticmethod
+    def get_custom_fields():
+        return {
+            'disk_quota': fields.Int(
+                required=True,
+                label="Disk quota",
+                description="Disk quota in bytes"
+            )
+        }
