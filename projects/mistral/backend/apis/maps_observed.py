86a2ffbaedc94d7f82359886af2cf169f1409e02
bchiavarini <b.chiavarini@cineca.it>
2020-04-22 14:41:28 +0200

    update to 0.3.0

--- projects/mistral/backend/apis/maps_observed.py
+++ projects/mistral/backend/apis/maps_observed.py
@@ -2,8 +2,7 @@
 
 from restapi.rest.definition import EndpointResource
 from restapi.exceptions import RestApiException
-from restapi.decorators import catch_error
-from restapi.protocols.bearer import authentication
+from restapi import decorators
 from restapi.utilities.htmlcodes import hcodes
 from mistral.services.arkimet import BeArkimet as arki
 from mistral.services.dballe import BeDballe as dballe
@@ -59,8 +58,8 @@ class MapsObservations(EndpointResource):
         }
     }
 
-    @catch_error()
-    @authentication.required()
+    @decorators.catch_errors()
+    @decorators.auth.required()
     def get(self, station_id=None):
         params = self.get_input()
         # ids = params.get('stations')
