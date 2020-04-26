3ce0dd351770f71f80220c4998e15666339445c5
Giuseppe Trotta <g.trotta@cineca.it>
2020-04-26 16:30:00 +0200

    initial setting of map and filter for the observed data

--- projects/mistral/frontend/app/components/maps/observation-maps/observation-maps.component.ts
+++ projects/mistral/frontend/app/components/maps/observation-maps/observation-maps.component.ts
@@ -1,12 +1,43 @@
 import {Component, OnInit, ViewChild, ElementRef} from "@angular/core";
+import {icon, latLng, Map, marker, point, polyline, tileLayer} from 'leaflet';
+import {NgbDateStruct, NgbPanelChangeEvent} from '@ng-bootstrap/ng-bootstrap';
+
+const MAP_CENTER = [41.879966, 12.280000];
 
 @Component({
     selector: 'app-observation-maps',
-    templateUrl: './observation-maps.component.html'
+    templateUrl: './observation-maps.component.html',
+    styleUrls: ['./observation-maps.component.css']
 })
 export class ObservationMapsComponent implements OnInit {
 
+    model: NgbDateStruct;
+
+    // base layers
+    streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        detectRetina: true,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+        detectRetina: true,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    options = {
+        layers: [
+            this.streetMaps
+        ],
+        zoom: 6,
+        center: MAP_CENTER
+    };
+
     ngOnInit() {
 
+    }
+
+    onMapReady(map: Map) {
+        // When the map is created, the ngx-leaflet directive calls
+        // onMapReady passing a reference to the map as an argument
+        // TODO
     }
 }
