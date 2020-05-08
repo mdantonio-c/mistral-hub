a14245fcc60c4df849e7ffff127af17d707ebe56
Mattia <m.dantonio81@gmail.com>
2020-05-08 18:09:44 +0200

    Added version and year information to the footer

--- projects/mistral/frontend/app/custom.footer.ts
+++ projects/mistral/frontend/app/custom.footer.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 import { environment } from '@rapydo/../environments/environment'
 
 @Component({
@@ -9,9 +10,11 @@ export class CustomFooterComponent {
 
   public project: string;
   public version: string;
+  public from_year: number = 2019;
+  public to_year = moment().year();
 
   constructor() {
-    var t = environment.projectTitle;
+    var t = environment.projectDescription;
     t = t.replace(/^'/, "");
     t = t.replace(/'$/, "");
     this.project = t;
