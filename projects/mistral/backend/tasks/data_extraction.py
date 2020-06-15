0ac4c242f3c36653f170d87ab5366c3eed7df3e2
bchiavarini <b.chiavarini@cineca.it>
2020-06-15 11:24:45 +0200

    issue 114: changed user's directories structure

--- projects/mistral/backend/tasks/data_extraction.py
+++ projects/mistral/backend/tasks/data_extraction.py
@@ -98,7 +98,7 @@ def data_extract(self, user_id, datasets, reftime=None, filters=None, postproces
 
             # create download user dir if it doesn't exist
             uuid = RequestManager.get_uuid(db, user_id)
-            user_dir = os.path.join(DOWNLOAD_DIR, uuid)
+            user_dir = os.path.join(DOWNLOAD_DIR, uuid, 'outputs')
             os.makedirs(user_dir, exist_ok=True)
 
             # check that the datasets are all under the same license
