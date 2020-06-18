aeee807feb26fac319b24827aadcd7dbdb73531e
Mattia <m.dantonio81@gmail.com>
2020-06-18 17:24:52 +0200

    Tmp fix

--- projects/mistral/backend/tests/test_api_data.py
+++ projects/mistral/backend/tests/test_api_data.py
@@ -1,7 +1,6 @@
 from unittest.mock import patch
 
 from restapi.connectors.celery import CeleryExt
-from restapi.services.detect import detector
 from restapi.tests import API_URI, BaseTests
 from restapi.utilities.htmlcodes import hcodes
 
@@ -16,14 +15,15 @@ class TestApp(BaseTests):
         r = self.mock_celery()
         assert r == 1
 
-        detector.init_services(
-            app=app, project_init=False, project_clean=False,
-        )
+    def test_endpoint_without_login(self, app, mock_celery, client):
+        # detector.init_services(
+        #     app=app, project_init=False, project_clean=False,
+        # )
 
-    celery = detector.get_service_instance("celery")
+        # celery = detector.get_service_instance("celery")
 
-    @patch.object(celery, "data_extract", side_effect=mock_celery)
-    def test_endpoint_without_login(self, mock_celery, client):
+        # patch.object(celery, "data_extract", side_effect=mock_celery)
+
         endpoint = f"{API_URI}/data"
         r = client.post(endpoint)
         assert r.status_code == hcodes.HTTP_BAD_UNAUTHORIZED
