d2404e36ae4aa7528a1e8b638f1aa300ddaa924d
Mattia <m.dantonio81@gmail.com>
2020-06-30 17:43:13 +0200

    Fix integration test

--- projects/mistral/frontend/integration/data-extraction.spec.ts
+++ projects/mistral/frontend/integration/data-extraction.spec.ts
@@ -1,68 +1,21 @@
-import "cypress-localstorage-commands";
+// This is to silence ESLint about undefined cy
+/*global cy*/
 
 describe("Data-extraction test", () => {
+  before(() => {
+    cy.login();
+  });
 
-    before(() => {
-        cy.clearLocalStorageSnapshot();
-        let credentials = {
-            username: Cypress.env('AUTH_DEFAULT_USERNAME'),
-            password: Cypress.env('AUTH_DEFAULT_PASSWORD')
-        }
-        cy.request('POST',  Cypress.env('API_URL') + 'auth/login', credentials)
-            .its('body')
-            .then(identity => {
-                cy.log('token', identity);
-                cy.setLocalStorage('token', identity);
-            });
+  // beforeEach(() => {
+  // });
 
-        cy.log('Login successful')
-        cy.getLocalStorage('token').then(
-          token => {
-            const options = {
-              method: 'GET',
-              url: Cypress.env('API_URL') + 'auth/profile',
-              'headers': {
-                'Authorization': `Bearer ${token}`
-              }
-            }
-
-            cy.request(options).then(response => {
-                cy.log('currentUser', response.body)
-                cy.setLocalStorage("currentUser", response.body);
-            });
-
-          }
-        );
-
-        cy.saveLocalStorage();
+  it("should visit dataset page", () => {
+    // now that we're logged in, we can visit
+    // any kind of restricted route!
+    cy.visit("app/data");
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq("/app/data/datasets");
     });
-
-    beforeEach(() => {
-      cy.restoreLocalStorage();
-    });
-
-    it("should exist identity in localStorage", () => {
-        cy.log(`${cy.getLocalStorage("token")}`);
-        cy.getLocalStorage("token").should("exist");
-        cy.getLocalStorage("token").then(token => {
-          cy.log("Identity token", token);
-        });
-    });
-
-    it("should exist currentUser in localStorage", () => {
-        cy.getLocalStorage("currentUser").should("exist");
-        cy.getLocalStorage("currentUser").then(profile => {
-          cy.log("currentUser", profile);
-        });
-    });
-
-    it('should visit dataset page', () => {
-        // now that we're logged in, we can visit
-        // any kind of restricted route!
-        cy.visit('app/data')
-        cy.location().should((location) => {
-            expect(location.pathname).to.eq("/app/data/datasets");
-        });
-        // cy.wait(2000)
-    });
+    // cy.wait(2000)
+  });
 });
