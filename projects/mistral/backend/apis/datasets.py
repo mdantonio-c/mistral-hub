1ca332a67ea8e7573468c0e22e306fe40b370c26
bchiavarini <b.chiavarini@cineca.it>
2020-07-07 17:41:51 +0200

    url field in licence and attribution table

--- projects/mistral/backend/apis/datasets.py
+++ projects/mistral/backend/apis/datasets.py
@@ -65,18 +65,22 @@ class Datasets(EndpointResource):
             for ds in datasets:
                 license_name = ds["license"]
                 license = db.License.query.filter_by(name=license_name).first()
-                ds["license_description"] = license.descr
-                group_license_id = license.group_license_id
-                gp_license = db.GroupLicense.query.filter_by(
-                    id=group_license_id
-                ).first()
-                ds["group_license"] = gp_license.name
-                ds["group_license_description"] = gp_license.descr
+                if license:
+                    ds["license_description"] = license.descr
+                    ds["license_url"] = license.url
+                    group_license_id = license.group_license_id
+                    gp_license = db.GroupLicense.query.filter_by(
+                        id=group_license_id
+                    ).first()
+                    ds["group_license"] = gp_license.name
+                    ds["group_license_description"] = gp_license.descr
 
                 attribution_name = ds["attribution"]
                 attribution = db.Attribution.query.filter_by(
                     name=attribution_name
                 ).first()
-                ds["attribution_description"] = attribution.descr
+                if attribution:
+                    ds["attribution_description"] = attribution.descr
+                    ds["attribution_url"] = attribution.url
 
         return self.response(datasets)
