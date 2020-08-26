1bdedb8a68699e0d9e86547c52b3f4d5514c66c8
Mattia <m.dantonio81@gmail.com>
2020-08-26 07:48:10 +0200

    Improvements on autocleaning task

--- projects/mistral/backend/tasks/requests_cleanup.py
+++ projects/mistral/backend/tasks/requests_cleanup.py
@@ -15,25 +15,29 @@ def automatic_cleanup(self):
         db = celery_app.get_service("sqlalchemy")
         users_settings = {}
         for u in db.User.query.all():
-            users_settings[u.uuid] = u.requests_expiration_days
+            if exp := u.requests_expiration_days:
+                users_settings[u.uuid] = timedelta(days=exp)
 
         now = datetime.now()
         requests = db.Request.query.all()
         for r in requests:
-            log.info("{} {}: {}", r.id, r.user_id, r.end_date.isoformat())
-            exp = users_settings.get(r.user_id, 0)
-            if not exp:
-                log.info("User {} disabled requests autocleaning", r.user_id)
+            if not (exp := users_settings.get(r.user_id)):
+                log.info("{}: user {} disabled requests autocleaning", r.id, r.user_id)
                 continue
 
-            exp = timedelta(days=exp)
+            if not r.end_date:
+                log.info("{} not completed yet?", r.id)
+                continue
 
-            if r.end_date < now - exp:
-                log.warning(
-                    "Request {} (completed on {}) should be deleted",
-                    r.id,
-                    r.end_date.isoformat(),
-                )
-                # repo.delete_request_record(db, user, request_id, DOWNLOAD_DIR)
+            if r.end_date > now - exp:
+                log.info("{} {}: {}", r.id, r.user_id, r.end_date.isoformat())
+                continue
+
+            log.warning(
+                "Request {} (completed on {}) should be deleted",
+                r.id,
+                r.end_date.isoformat(),
+            )
+            # repo.delete_request_record(db, user, request_id, DOWNLOAD_DIR)
 
         return "Autocleaning task executed!"
