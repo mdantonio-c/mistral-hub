031945320faf4edaa90d5874aaca122c017144ea
Giuseppe Trotta <g.trotta@cineca.it>
2020-09-09 09:45:10 +0200

    remove platform in meteo tiles check API

--- projects/mistral/backend/endpoints/tiles.py
+++ projects/mistral/backend/endpoints/tiles.py
@@ -12,8 +12,8 @@ MEDIA_ROOT = "/meteo/"
 
 RUNS = ["00", "12"]
 RESOLUTIONS = ["lm2.2", "lm5"]
-PLATFORMS = ["GALILEO", "MEUCCI"]
-DEFAULT_PLATFORM = os.environ.get("PLATFORM", "GALILEO")
+# PLATFORMS = ["GALILEO", "MEUCCI"]
+# DEFAULT_PLATFORM = os.environ.get("PLATFORM", "GALILEO")
 
 
 def check_platform_availability(platform):
@@ -45,25 +45,6 @@ class TilesEndpoint(EndpointResource):
     )
     def get(self, res, run=None):
         ready_file = None
-        # check the available platform by looking first at the default one
-        log.debug(f"PLATFORMS: {PLATFORMS}")
-        log.debug(f"DEFAULT PLATFORM: {DEFAULT_PLATFORM}")
-        platforms_to_be_check = [DEFAULT_PLATFORM] + list(
-            set(PLATFORMS) - {DEFAULT_PLATFORM}
-        )
-        log.debug(f"platform to be check {platforms_to_be_check}")
-        for p in platforms_to_be_check:
-            if not check_platform_availability(p):
-                log.warning(f"platform {p} not available")
-                continue
-            else:
-                platform = p
-                break
-        else:
-            raise RestApiException(
-                "Map service is currently unavailable", hcodes.HTTP_SERVICE_UNAVAILABLE
-            )
-
         area = "Italia" if res == "lm2.2" else "Area_Mediterranea"
 
         # check for run param: if not provided get the "last" run available
@@ -71,9 +52,7 @@ class TilesEndpoint(EndpointResource):
             log.debug("No run param provided: look for the last run available")
             ready_files = [
                 x
-                for x in (
-                    self._get_ready_file(platform, area, r, res) for r in ["00", "12"]
-                )
+                for x in (self._get_ready_file(area, r, res) for r in ["00", "12"])
                 if x is not None
             ]
             try:
@@ -81,21 +60,18 @@ class TilesEndpoint(EndpointResource):
             except ValueError:
                 log.warning("No Run is available: .READY file not found")
         else:
-            ready_file = self._get_ready_file(platform, area, run, res)
-            if not ready_file:
-                raise RestApiException(
-                    f"No .READY file found for RUN at {run}:00",
-                    status_code=hcodes.HTTP_BAD_NOTFOUND,
-                )
+            ready_file = self._get_ready_file(area, run, res)
+        if not ready_file:
+            raise RestApiException(
+                "No .READY file found", status_code=hcodes.HTTP_BAD_NOTFOUND,
+            )
 
-        data = {"reftime": ready_file[:10], "platform": platform}
+        data = {"reftime": ready_file[:10], "platform": None}
         return self.response(data)
 
-    def _get_ready_file(self, platform, area, run, res):
+    def _get_ready_file(self, area, run, res):
         # e.g. Tiles-00-lm2.2.web
-        self.base_path = os.path.join(
-            MEDIA_ROOT, platform, "PROD", f"Tiles-{run}-{res}.web"
-        )
+        self.base_path = os.path.join(MEDIA_ROOT, "PROD", f"Tiles-{run}-{res}.web")
         ready_path = os.path.join(self.base_path, area)
         log.debug("ready_path: {}", ready_path)
 
