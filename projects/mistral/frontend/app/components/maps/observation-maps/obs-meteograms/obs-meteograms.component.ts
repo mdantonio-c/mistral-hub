dbec436e56d946b5791c8522c42c5601b1245c7d
Giuseppe Trotta <g.trotta@cineca.it>
2020-09-14 15:15:21 +0200

    add descriptions and user unit for meteograms

--- projects/mistral/frontend/app/components/maps/observation-maps/obs-meteograms/obs-meteograms.component.ts
+++ projects/mistral/frontend/app/components/maps/observation-maps/obs-meteograms/obs-meteograms.component.ts
@@ -27,6 +27,13 @@ export class ObsMeteogramsComponent implements OnInit {
   report: Observation[];
   loading: boolean = false;
 
+  // product info
+  varcode: string;
+  product: string;
+  level: string;
+  timerange: string;
+  userunit: string;
+
   // chart options
   colorScheme = {
     domain: ["#5AA454", "#E44D25", "#CFC0BB", "#7aa3e5", "#a8385d", "#aae3f5"],
@@ -78,6 +85,18 @@ export class ObsMeteogramsComponent implements OnInit {
         (data: Observation[]) => {
           this.report = data;
           // console.log(this.report);
+          // get product info
+          if (data.length !== 0) {
+            let obs = data[0];
+            this.product = obs.products[0].description;
+            this.varcode = obs.products[0].varcode;
+            if (filter.level) {
+              this.level = obs.products[0].values[0].level_desc;
+            }
+            if (filter.timerange) {
+              this.timerange = obs.products[0].values[0].timerange_desc;
+            }
+          }
           let multi = this.normalize(data);
           Object.assign(this, { multi });
         },
