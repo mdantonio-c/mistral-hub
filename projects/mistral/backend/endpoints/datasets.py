974c78ab3c86bc46f5916acc5401a225bb538054
bchiavarini <b.chiavarini@cineca.it>
2020-10-22 18:24:21 +0200

    filtering datasets page by user authorization

--- projects/mistral/backend/endpoints/datasets.py
+++ projects/mistral/backend/endpoints/datasets.py
@@ -43,6 +43,38 @@ class Datasets(EndpointResource):
             raise RestApiException(
                 "Error loading the datasets", status_code=hcodes.HTTP_SERVER_ERROR
             )
+
+        db = self.get_service_instance("sqlalchemy")
+        user = self.get_user()
+        # get user authorized licence group
+        user_license_groups = [lg.name for lg in user.group_license]
+        user_datasets_auth = [ds.name for ds in user.datasets]
+
+        for ds in datasets:
+            ds_entry = db.Datasets.query.filter_by(name=ds["name"]).first()
+            license = db.License.query.filter_by(id=ds_entry.license_id).first()
+            group_license = db.GroupLicense.query.filter_by(
+                id=license.group_license_id
+            ).first()
+            # check the licence group authorization for the user
+            if group_license.name not in user_license_groups:
+                # looking for exception: check the authorized datasets
+                if ds["name"] not in user_datasets_auth:
+                    # remove the dataset from the response
+                    datasets.remove(ds)
+                    continue
+
+            if licenceSpecs:
+                attribution = db.Attribution.query.filter_by(
+                    id=ds_entry.attribution_id
+                ).first()
+                ds["license_description"] = license.descr
+                ds["license_url"] = license.url
+                ds["group_license"] = group_license.name
+                ds["group_license_description"] = group_license.descr
+                ds["attribution_description"] = attribution.descr
+                ds["attribution_url"] = attribution.url
+
         if dataset_name is not None:
             # retrieve dataset by name
             log.debug("retrieve dataset by name '{}'", dataset_name)
@@ -55,23 +87,5 @@ class Datasets(EndpointResource):
                     status_code=hcodes.HTTP_BAD_NOTFOUND,
                 )
             return self.response(matched_ds)
-        if licenceSpecs:
-            db = self.get_service_instance("sqlalchemy")
-            for ds in datasets:
-                # get the dataset entry
-                ds_entry = db.Datasets.query.filter_by(name=ds["name"]).first()
-                license = db.License.query.filter_by(id=ds_entry.license_id).first()
-                attribution = db.Attribution.query.filter_by(
-                    id=ds_entry.attribution_id
-                ).first()
-                ds["license_description"] = license.descr
-                ds["license_url"] = license.url
-                group_license = db.GroupLicense.query.filter_by(
-                    id=license.group_license_id
-                ).first()
-                ds["group_license"] = group_license.name
-                ds["group_license_description"] = group_license.descr
-                ds["attribution_description"] = attribution.descr
-                ds["attribution_url"] = attribution.url
 
         return self.response(datasets)
