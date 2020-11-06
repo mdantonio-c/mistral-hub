5dcba5ae03ae84a625f9e869bee44c2d252ceb51
bchiavarini <b.chiavarini@cineca.it>
2020-11-06 16:27:22 +0100

    datasets tests extended to new functionalities

--- projects/mistral/backend/tests/test_api_dataset.py
+++ projects/mistral/backend/tests/test_api_dataset.py
@@ -4,8 +4,7 @@ import json
 from restapi.services.detect import detector
 from restapi.tests import API_URI, BaseTests
 from restapi.utilities.htmlcodes import hcodes
-
-# from restapi.utilities.logs import log
+from restapi.utilities.logs import log
 
 __author__ = "Beatrice Chiavarini (b.chiavarini@cineca.it)"
 
@@ -14,16 +13,19 @@ class TestApp(BaseTests):
     def test_endpoint_without_login(self, client, faker):
         endpoint = API_URI + "/datasets"
         r = client.get(endpoint)
-        assert r.status_code == hcodes.HTTP_BAD_UNAUTHORIZED
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        # check that there aren't authorization specs
+        data = self.get_content(r)
+        assert "authorized" not in data[0]
 
         endpoint = API_URI + "/datasets/lm5"
         r = client.get(endpoint)
-        assert r.status_code == hcodes.HTTP_BAD_UNAUTHORIZED
+        assert r.status_code == hcodes.HTTP_OK_BASIC
 
         # trying a dataset that doesn't exists
         endpoint = API_URI + "/datasets/" + faker.pystr()
         r = client.get(endpoint)
-        assert r.status_code == hcodes.HTTP_BAD_UNAUTHORIZED
+        assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
 
     def test_endpoint_with_login(self, client, faker):
 
@@ -33,17 +35,34 @@ class TestApp(BaseTests):
         data = self.buildData(schema)
         # get the group license id for user authorization
         obj = detector.get_debug_instance("sqlalchemy")
-        group_lic_to_auth = obj.GroupLicense.query.filter_by(
-            name="CCBY_COMPLIANT"
-        ).first()
         # get the special dataset id for user authorization
         dataset_to_auth = obj.Datasets.query.filter_by(
             name="sa_dataset_special"
         ).first()
 
+        # create a fake copyright group
+        fake_private_group = obj.GroupLicense(
+            name="private_group", descr="mock private_group", is_public=False
+        )
+        obj.session.add(fake_private_group)
+        obj.session.flush()
+        # create a fake copyright license
+        fake_private_lic = obj.License(
+            name="private auth license",
+            descr="mock private license",
+            group_license_id=fake_private_group.id,
+        )
+        obj.session.add(fake_private_lic)
+        obj.session.flush()
+        # link the dataset to the fake copyright license
+        unauth_dataset = obj.Datasets.query.filter_by(name="sa_dataset").first()
+        unauth_dataset.license_id = fake_private_lic.id
+        obj.session.add(unauth_dataset)
+        dataset_to_auth.license_id = fake_private_lic.id
+        obj.session.add(dataset_to_auth)
+        obj.session.commit()
+
         data["is_active"] = True
-        data["group_license"] = [str(group_lic_to_auth.id)]
-        data["group_license"] = json.dumps(data["group_license"])
         data["datasets"] = [str(dataset_to_auth.id)]
         data["datasets"] = json.dumps(data["datasets"])
 
@@ -83,12 +102,22 @@ class TestApp(BaseTests):
         # trying a dataset in an unauthorized license group
         endpoint = API_URI + "/datasets/sa_dataset"
         r = client.get(endpoint, headers=self.get("auth_header"))
-        assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        response_data = self.get_content(r)
+        # the dataset is not public
+        assert response_data["is_public"] is False
+        # the user cannot access
+        assert response_data["authorized"] is False
 
         # trying an authorized dataset in an unauthorized license group
         endpoint = API_URI + "/datasets/sa_dataset_special"
         r = client.get(endpoint, headers=self.get("auth_header"))
         assert r.status_code == hcodes.HTTP_OK_BASIC
+        response_data = self.get_content(r)
+        # the dataset is not public
+        assert response_data["is_public"] is False
+        # the user can access
+        assert response_data["authorized"] is True
 
         # trying error dataset
         endpoint = API_URI + "/datasets/error"
@@ -103,3 +132,16 @@ class TestApp(BaseTests):
         # delete the fake user
         r = client.delete(f"{API_URI}/admin/users/{uuid}", headers=admin_headers)
         assert r.status_code == 204
+
+        # delete the mock licence and licence group
+        obj.session.delete(fake_private_group)
+        obj.session.delete(fake_private_lic)
+
+        # get a new existing license to the mock private datasets
+        correct_license = obj.License.query.filter_by().first()
+        unauth_dataset.license_id = correct_license.id
+        obj.session.add(unauth_dataset)
+        dataset_to_auth.license_id = correct_license.id
+        obj.session.add(dataset_to_auth)
+
+        obj.session.commit()
