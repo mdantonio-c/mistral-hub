408433dfd16791fdf7c07652734d577b9890683a
Mattia <m.dantonio@cineca.it>
2021-07-07 13:00:09 +0200

    Karma test fix

--- projects/mistral/frontend/app/components/dashboard/dashboard.component.spec.ts
+++ projects/mistral/frontend/app/components/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { Component, DebugElement } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { By } from "@angular/platform-browser";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
@@ -45,7 +46,7 @@ describe("DashboardComponent", () => {
         StubSchedulesComponent,
         StubHourlyReportComponent,
       ],
-      imports: [NgbModule],
+      imports: [BrowserAnimationsModule, NgbModule],
     }).compileComponents();
   }));
 
