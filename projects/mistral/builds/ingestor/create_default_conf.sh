1392afe77fd77881389f02ff9b39a611e7b55fe5
Mattia <m.dantonio@cineca.it>
2021-08-06 11:17:34 +0200

    Added create_default_conf script to nifi

---
+++ projects/mistral/builds/ingestor/create_default_conf.sh
@@ -0,0 +1,8 @@
+if [ -z "$(ls ${NIFI_HOME}/conf)" ]; then
+   echo "Default conf is missing, copying it..."
+   cp ${NIFI_HOME}/default-conf/* ${NIFI_HOME}/conf/
+   echo "Default conf copied"
+else
+   echo "Default conf already initialized"
+fi
+
