ce77cf11914651bd7a50430204442d62c340e9d9
Mattia <m.dantonio@cineca.it>
2021-11-03 10:06:14 +0100

    Fixes to make nifi build to run the entrypoint at root level to be able to automatically generate certificates

--- projects/mistral/builds/ingestor/docker-entrypoint.sh
+++ projects/mistral/builds/ingestor/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+# add cert creation here
+
 if [ -z "$(ls ${NIFI_HOME}/conf)" ]; then
    echo "Default conf is missing, copying it..."
    cp ${NIFI_HOME}/default-conf/* ${NIFI_HOME}/conf/
+   chown -R nifi:nifi ${NIFI_HOME}/conf/
    echo "Default conf copied"
 else
    echo "Default conf already initialized"
 fi
 
-/opt/nifi/scripts/start.sh
+su -p nifi -c '/opt/nifi/scripts/start.sh'
