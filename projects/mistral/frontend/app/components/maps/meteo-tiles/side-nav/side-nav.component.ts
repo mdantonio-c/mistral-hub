0162ca1136548976a9af62bd69e984a76e04f2ff
Giuseppe Trotta <g.trotta@cineca.it>
2021-11-08 01:01:40 +0100

    bind precipitation forecasts for different step hours in the map sidenav menu

--- projects/mistral/frontend/app/components/maps/meteo-tiles/side-nav/side-nav.component.ts
+++ projects/mistral/frontend/app/components/maps/meteo-tiles/side-nav/side-nav.component.ts
@@ -38,6 +38,13 @@ export class SideNavComponent implements OnInit, OnDestroy {
 
   public isCollapsed = false;
   public availableDatasets = DATASETS;
+  public precipitationHours: any = [
+    { hour: 1, selected: false },
+    { hour: 3, selected: false },
+    { hour: 6, selected: false },
+    { hour: 12, selected: false },
+    { hour: 24, selected: false },
+  ];
 
   subscription: Subscription = new Subscription();
   routeDataSubscription!: Subscription;
@@ -97,37 +104,49 @@ export class SideNavComponent implements OnInit, OnDestroy {
     }
   }
 
-  toggleLayer(event, layerId: string) {
+  toggleLayer(event: Event, layerId: string, isMultiLayers: boolean = false) {
     event.preventDefault();
-    for (const [key, layer] of Object.entries(this.overlays)) {
-      if (layerId === this._toLayerCode(key)) {
-        // this.onLayerChange.emit(layer);
-        this.onLayerChange.emit({
-          layer: layer,
-          name: this._toLayerTitle(layerId),
-        });
-        // activate / deactivate
-        let el = this.el.nativeElement.querySelector(`.${layerId}`);
-        el.classList.contains("attivo")
-          ? this.renderer.removeClass(el, "attivo")
-          : this.renderer.addClass(el, "attivo");
-        break;
+    // FIXME can we do better with multi layer products? (i.e. prp)
+    const fromActiveState: boolean = (
+      event.target as HTMLInputElement
+    ).className.includes("attivo");
+    const op = fromActiveState ? "remove" : "add";
+    if (layerId === "prp") {
+      if (op === "remove") {
+        // reset precipitation hours
+        this.precipitationHours.forEach((e) => (e.selected = false));
+      } else {
+        const found = this.precipitationHours.find((e) => e.selected);
+        if (!found) {
+          // select default
+          this.precipitationHours[0].selected = true;
+        }
       }
     }
+    for (const [key, layer] of Object.entries(this.overlays)) {
+      if (layerId === this._toLayerCode(key)) {
+        // let op = (this.map.hasLayer(layer)) ? 'remove' : 'add';
+        if (this.map.hasLayer(layer) && op === "remove") {
+          this.onLayerChange.emit({
+            layer: layer,
+            name: key,
+          });
+        }
+        if (op === "add") {
+          this.onLayerChange.emit({
+            layer: layer,
+            name: key,
+          });
+          break;
+        }
+      }
+    }
+    // update active class
+    let el = this.el.nativeElement.querySelector(`.${layerId}`);
+    el.classList.contains("attivo")
+      ? this.renderer.removeClass(el, "attivo")
+      : this.renderer.addClass(el, "attivo");
   }
-
-  /**
-   * Adds an overlay (checkbox entry) with the given name to the control.
-   * @param layer
-   * @param name
-   */
-  addOverlay(layer, name: string) {}
-
-  /**
-   * Remove the given layer from the control.
-   * @param layer
-   */
-  removeLayer(layer) {}
 
   /**
    * Custom mapper for overlay titles into component codes
@@ -142,18 +161,31 @@ export class SideNavComponent implements OnInit, OnDestroy {
         return "prs";
       case DP.RH:
         return "rh";
-      case DP.PREC1P || DP.PREC3P || DP.PREC6P || DP.PREC12P || DP.PREC24P:
+      case DP.PREC1P:
+      case DP.PREC3P:
+      case DP.PREC6P:
+      case DP.PREC12P:
+      case DP.PREC24P:
         return "prp";
-      case DP.SF1 || DP.SF3 || DP.SF6 || DP.SF12 || DP.SF24:
+      case DP.SF1:
+      case DP.SF3:
+      case DP.SF6:
+      case DP.SF12:
+      case DP.SF24:
         return "sf";
-      case DP.LCC || DP.MCC || DP.HCC:
+      case DP.LCC:
+      case DP.MCC:
+      case DP.HCC:
         return "cc";
       default:
         return null;
     }
   }
 
-  private _toLayerTitle(code: string): string | null {
+  private _toLayerTitle(
+    code: string,
+    lvl: number | null = null
+  ): string | null {
     switch (code) {
       case "t2m":
         return DP.TM2;
@@ -162,6 +194,18 @@ export class SideNavComponent implements OnInit, OnDestroy {
       case "rh":
         return DP.RH;
       case "prp":
+        switch (lvl) {
+          case 1:
+            return DP.PREC1P;
+          case 3:
+            return DP.PREC3P;
+          case 6:
+            return DP.PREC6P;
+          case 12:
+            return DP.PREC12P;
+          case 24:
+            return DP.PREC24P;
+        }
         return DP.PREC1P;
       case "sf":
         return DP.SF1;
@@ -179,5 +223,54 @@ export class SideNavComponent implements OnInit, OnDestroy {
 
   private _escapeChars(input: string): string {
     return input.replace(/\./g, "\\.");
+  }
+
+  /**
+   * Activate / Deactivate a layer with sub levels
+   * @param event
+   * @param target
+   * @param layerId
+   */
+  changePrecipitationHour(event: Event, target, layerId: string) {
+    target.selected = !target.selected;
+    let activeHoursCount = this.precipitationHours
+      .filter((h) => h.selected)
+      .reduce((accumulator) => {
+        return accumulator + 1;
+      }, 0);
+    if (activeHoursCount === 0) {
+      // prevent unchecking last active hour
+      target.selected = !target.selected;
+      (event.target as HTMLInputElement).checked = true;
+      // do nothing
+      return;
+    }
+    // console.log(`activate layer ${layerId}, hour ${target.hour}`);
+    for (const [key, layer] of Object.entries(this.overlays)) {
+      if (
+        layerId === this._toLayerCode(key) &&
+        key.includes(`(${target.hour}h)`)
+      ) {
+        this.onLayerChange.emit({
+          layer: layer,
+          name: this._toLayerTitle(layerId, target.hour),
+        });
+        break;
+      }
+    }
+  }
+
+  /**
+   *
+   * @param layerId
+   */
+  isLayerActive(layerId: string): boolean {
+    if (!this.overlays) return false;
+    for (const [key, layer] of Object.entries(this.overlays)) {
+      if (layerId === this._toLayerCode(key)) {
+        return this.map.hasLayer(layer) ? true : false;
+      }
+    }
+    return false;
   }
 }
