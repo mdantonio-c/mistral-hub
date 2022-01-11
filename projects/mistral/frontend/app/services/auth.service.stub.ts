ef9a7ceb66482fdd82c05d2b7f9e561790f58491
Mattia D'Antonio <m.dantonio@cineca.it>
2022-01-11 10:16:24 +0100

    Compatibility fix

--- projects/mistral/frontend/app/services/auth.service.stub.ts
+++ projects/mistral/frontend/app/services/auth.service.stub.ts
@@ -2,13 +2,18 @@ import { Injectable } from "@angular/core";
 import { User } from "@rapydo/types";
 import { MockUser } from "@app/services/data.mock";
 import { AuthService } from "@rapydo/services/auth";
-import { NotificationService } from "@rapydo/services/notification";
 import { ApiService } from "@rapydo/services/api";
+import { LocalStorageService } from "@rapydo/services/localstorage";
+import { NotificationService } from "@rapydo/services/notification";
 
 @Injectable()
 export class AuthServiceStub extends AuthService {
   constructor() {
-    super({} as ApiService, {} as NotificationService);
+    super(
+      {} as LocalStorageService,
+      {} as ApiService,
+      {} as NotificationService
+    );
   }
 
   getUser(): User {
