6de4a68d2053ec867f62bb3a9bdf9bef14008f5b
bchiavarini <b.chiavarini@cineca.it>
2022-02-02 19:00:48 +0100

    setup nifi template command: setting up the controllers

--- projects/mistral/commands/setup_nifi_template.py
+++ projects/mistral/commands/setup_nifi_template.py
@@ -18,7 +18,7 @@ def get_nifi_token():
     r = requests.post(token_url, data=params, headers=headers)
     if r.status_code != 200:
         log.error(
-            f"Error in accessing Nifi API: status {r.status_code}, response {r.text}"
+            f"Error in accessing Nifi API: Status: {r.status_code}, Response: {r.text}"
         )
         return None
     return r.text()
@@ -46,7 +46,7 @@ def setup_nifi_template(
     r = requests.get(access_url)
     if r.status_code != 200:
         log.error(
-            f"Error in accessing Nifi API: status {r.status_code}, response {r.text}"
+            f"Error in accessing Nifi API: Status: {r.status_code}, Response: {r.text}"
         )
         return
     res = r.json()
@@ -64,7 +64,7 @@ def setup_nifi_template(
     r = requests.get(resources_url, headers=headers)
     if r.status_code != 200:
         log.error(
-            f"Error in accessing Nifi API: status {r.status_code}, response {r.text}"
+            f"Error in accessing Nifi API: Status: {r.status_code}, Response: {r.text}"
         )
         return
     resources = r.json()
@@ -83,7 +83,7 @@ def setup_nifi_template(
         r = requests.get(context_url, headers=headers)
         if r.status_code != 200:
             log.error(
-                f"Error in accessing parameter contexts list: status {r.status_code}, response {r.text}"
+                f"Error in accessing parameter contexts list: Status: {r.status_code}, Response: {r.text}"
             )
             return
         context_list = r.json()
@@ -102,7 +102,7 @@ def setup_nifi_template(
         r = requests.get(p_group_url, headers=headers)
         if r.status_code != 200:
             log.error(
-                f"Error in getting process group entity of the tempolate process group: status {r.status_code}, response {r.text}"
+                f"Error in getting process group entity of the tempolate process group: Status: {r.status_code}, Response: {r.text}"
             )
             return
         process_group_entities.append(r.json())
@@ -114,7 +114,7 @@ def setup_nifi_template(
         r = requests.get(process_groups_url, headers=headers)
         if r.status_code != 200:
             log.error(
-                f"Error in accessing process group children list: status {r.status_code}, response {r.text}"
+                f"Error in accessing process group children list: Status: {r.status_code}, Response: {r.text}"
             )
             return
         res = r.json()
@@ -133,9 +133,112 @@ def setup_nifi_template(
             r = requests.put(p_group["uri"], data=data, headers=p_group_header)
             if r.status_code != 200:
                 log.error(
-                    f"Error in setting the parameter context: status {r.status_code}, response {r.text}"
+                    f"Error in setting the parameter context: Status: {r.status_code}, Response: {r.text}"
                 )
                 return
         log.info(
             f"Parameter context '{context}' successfully assigned to all template '{template}' process groups"
         )
+
+    log.info("Setup controllers")
+    # get all the controllers of the template
+    get_controller_url = (
+        f"{NIFI_API_URI}/flow{process_group['identifier']}/controller-services"
+    )
+    r = requests.get(get_controller_url, headers=headers)
+    if r.status_code != 200:
+        log.error(
+            f"Error in accessing template controller list: Status: {r.status_code}, Response: {r.text}"
+        )
+        return
+    res = r.json()
+    controllers_id_list: List[str] = []
+    for el in res["controllerServices"]:
+        controllers_id_list.append(el["id"])
+
+    controller_header = {**headers}
+    controller_header["Content-Type"] = "application/json"
+    for c_id in controllers_id_list:
+        # check the status
+        controller_url = f"{NIFI_API_URI}/controller-services/{c_id}"
+        r = requests.get(controller_url, headers=headers)
+        if r.status_code != 200:
+            log.error(
+                f"Error in accessing the controller element: Status: {r.status_code}, Response: {r.text}"
+            )
+            return
+        controller_el = r.json()
+        to_update = False
+        if controller_el["status"]["validationStatus"] == "INVALID":
+            to_update = True
+            # check the passwords
+            log.info(f"Checking passwords for {controller_el['component']['name']}")
+            password_params: Dict[str, str] = {}
+            for p, values in controller_el["component"]["properties"].items():
+                if "password" in p.lower():
+                    new_p = typer.prompt(
+                        f"Value for '{p}' in {controller_el['component']['name']} ? (type q to skip)"
+                    )
+                    if new_p != "q":
+                        password_params[p] = new_p
+            # apply the changes
+            if password_params:
+
+                if controller_el["status"]["runStatus"] != "DISABLED":
+                    log.debug("entro qui")
+                    # controller status has to be set to disabled in order to be allowed to modify its properties
+                    status_body = {
+                        "revision": controller_el["revision"],
+                        "state": "DISABLED",
+                    }
+                    status_url = f"{NIFI_API_URI}/controller-services/{c_id}/run-status"
+                    dis_r = requests.put(
+                        status_url,
+                        data=json.dumps(status_body),
+                        headers=controller_header,
+                    )
+                    if dis_r.status_code != 200:
+                        log.error(
+                            f"Error in disabling the controller element to update the passwords: Status: {r.status_code}, Response: {r.text}"
+                        )
+                        return
+                    controller_el = dis_r.json()
+
+                controller_el["component"]["properties"] = password_params
+                r = requests.put(
+                    controller_url,
+                    data=json.dumps(controller_el),
+                    headers=controller_header,
+                )
+                if r.status_code != 200:
+                    log.error(
+                        f"Error in updating {controller_el['component']['name']} : Status: {r.status_code}, Response: {r.text}"
+                    )
+                    return
+                log.info(
+                    f"Passwords for {controller_el['component']['name']} successfully updated"
+                )
+        if to_update:
+            # update the controller element
+            controller_el = r.json()
+            # check if the problem is solved
+            if controller_el["status"]["validationStatus"] == "INVALID":
+                log.warning(
+                    f"Validation problems with controller {controller_el['component']['name']} (id: {controller_el['component']['id']}):{controller_el['component']['validationErrors']}."
+                )
+                log.warning("Enabling Skipped")
+                continue
+        if controller_el["status"]["runStatus"] == "DISABLED":
+            log.info(f"Enabling {controller_el['component']['name']}")
+            status_body = {"revision": controller_el["revision"], "state": "ENABLED"}
+            status_url = f"{NIFI_API_URI}/controller-services/{c_id}/run-status"
+            r = requests.put(
+                status_url, data=json.dumps(status_body), headers=controller_header
+            )
+            if r.status_code != 200:
+                log.error(
+                    f"Error in enabling the controller element: Status: {r.status_code}, Response: {r.text}"
+                )
+                return
+
+    log.info("Controllers successfully set up")
