9554752ff4f1160d974682088017fcc8eae18cd2
bchiavarini <b.chiavarini@cineca.it>
2022-02-03 16:04:42 +0100

    setup nifi template command: updating processors passwords

--- projects/mistral/commands/setup_nifi_template.py
+++ projects/mistral/commands/setup_nifi_template.py
@@ -200,7 +200,7 @@ def setup_nifi_template(
                     )
                     if dis_r.status_code != 200:
                         log.error(
-                            f"Error in disabling the controller element to update the passwords: Status: {r.status_code}, Response: {r.text}"
+                            f"Error in disabling the controller element to update the passwords: Status: {dis_r.status_code}, Response: {dis_r.text}"
                         )
                         return
                     controller_el = dis_r.json()
@@ -225,9 +225,8 @@ def setup_nifi_template(
             # check if the problem is solved
             if controller_el["status"]["validationStatus"] == "INVALID":
                 log.warning(
-                    f"Validation problems with controller {controller_el['component']['name']} (id: {controller_el['component']['id']}):{controller_el['component']['validationErrors']}."
+                    f"Skipping enabling controller {controller_el['component']['name']} (id: {controller_el['component']['id']}).Validation errors: {controller_el['component']['validationErrors']}."
                 )
-                log.warning("Enabling Skipped")
                 continue
         if controller_el["status"]["runStatus"] == "DISABLED":
             log.info(f"Enabling {controller_el['component']['name']}")
@@ -243,3 +242,113 @@ def setup_nifi_template(
                 return
 
     log.info("Controllers successfully set up")
+
+    # check if there are remaining invalid processors
+    p_group_url = f"{NIFI_API_URI}{process_group['identifier']}"
+    r = requests.get(p_group_url, headers=headers)
+    if r.status_code != 200:
+        log.error(
+            f"Error in getting process group entity of the tempolate process group: Status: {r.status_code}, Response: {r.text}"
+        )
+        return
+    res = r.json()
+    processors_to_check: List[str] = []
+    if res["invalidCount"] > 0:
+        log.info(f"{res['invalidCount']} processors are invalid")
+        # get the processor-groups elements
+        flow_url = f"{NIFI_API_URI}/flow{process_group['identifier']}"
+        flow_r = requests.get(flow_url, headers=headers)
+        if flow_r.status_code != 200:
+            log.error(
+                f"Error in getting process group flow of the tempolate process group: Status: {flow_r.status_code}, Response: {flow_r.text}"
+            )
+            return
+        flow_res = flow_r.json()
+        # look for invalid processors
+        for p in flow_res["processGroupFlow"]["flow"]["processors"]:
+            if p["status"]["runStatus"] == "Invalid":
+                processors_to_check.append(p["id"])
+        # check if all invalid processors have been found
+        if len(processors_to_check) < res["invalidCount"]:
+            log.info(
+                f"{len(processors_to_check)} found in the main process group. Check for more invalid processors in children process groups."
+            )
+            for p_group in flow_res["processGroupFlow"]["flow"]["processGroups"]:
+                p_r = requests.get(p_group["uri"], headers=headers)
+                if p_r.status_code != 200:
+                    log.error(
+                        f"Error in getting process group children: Status: {p_r.status_code}, Response: {p_r.text}"
+                    )
+                    return
+                p_res = p_r.json()
+                if p_res["invalidCount"] > 0:
+                    # get the processor-groups elements
+                    p_flow_url = f"{NIFI_API_URI}/flow/process-groups/{p_group['id']}"
+                    p_flow_r = requests.get(p_flow_url, headers=headers)
+                    if p_flow_r.status_code != 200:
+                        log.error(
+                            f"Error in getting process group flow children of the template process group: Status: {p_flow_r.status_code}, Response: {p_flow_r.text}"
+                        )
+                        return
+                    p_flow_res = p_flow_r.json()
+                    # look for invalid processors
+                    for p in p_flow_res["processGroupFlow"]["flow"]["processors"]:
+                        if p["status"]["runStatus"] == "Invalid":
+                            processors_to_check.append(p["id"])
+                    # check if all invalid processors have been found
+                    if len(processors_to_check) == res["invalidCount"]:
+                        # all invalid processors have been found
+                        break
+    if processors_to_check:
+        processor_header = {**headers}
+        processor_header["Content-Type"] = "application/json"
+        for p_id in processors_to_check:
+            processor_url = f"{NIFI_API_URI}/processors/{p_id}"
+            r = requests.get(processor_url, headers=headers)
+            if r.status_code != 200:
+                log.error(
+                    f"Error in accessing the processor element: Status: {r.status_code}, Response: {r.text}"
+                )
+                return
+            processor_el = r.json()
+            log.info(
+                f"Checking passwords for processor {processor_el['component']['name']}"
+            )
+            password_params: Dict[str, str] = {}
+            for p, values in processor_el["component"]["config"]["properties"].items():
+                if "password" in p.lower():
+                    new_p = typer.prompt(
+                        f"Value for '{p}' in {processor_el['component']['name']} ? (type q to skip)"
+                    )
+                    if new_p != "q":
+                        password_params[p] = new_p
+            # apply the changes
+            if password_params:
+                processor_el["component"]["config"]["properties"] = password_params
+                r = requests.put(
+                    processor_url,
+                    data=json.dumps(processor_el),
+                    headers=processor_header,
+                )
+                if r.status_code != 200:
+                    log.error(
+                        f"Error in updating {processor_el['component']['name']} : Status: {r.status_code}, Response: {r.text}"
+                    )
+                    return
+                log.info(
+                    f"Passwords for {processor_el['component']['name']} successfully updated"
+                )
+        # check if the problem is solved
+        r = requests.get(p_group_url, headers=headers)
+        if r.status_code != 200:
+            log.error(
+                f"Error in getting process group entity of the tempolate process group: Status: {r.status_code}, Response: {r.text}"
+            )
+            return
+        res = r.json()
+        if res["invalidCount"] > 0:
+            log.warning(f"{res['invalidCount']} processors are still invalid")
+        else:
+            log.info("All the processors are valid")
+
+    log.info(f"set up of '{template}' -- COMPLETED")
