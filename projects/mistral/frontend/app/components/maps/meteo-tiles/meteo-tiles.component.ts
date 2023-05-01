12e2a50179d60ce7708c4b974315e127cd311f02
Giuseppe Trotta <g.trotta@cineca.it>
2023-05-01 12:25:44 +0200

    bug fix: manage max zoom level 7 for lm5 in meteo-tiles map

--- projects/mistral/frontend/app/components/maps/meteo-tiles/meteo-tiles.component.ts
+++ projects/mistral/frontend/app/components/maps/meteo-tiles/meteo-tiles.component.ts
@@ -114,7 +114,8 @@ export class MeteoTilesComponent implements OnInit {
   };
   options = {
     zoomControl: false,
-    zoom: MIN_ZOOM,
+    minZoom: MIN_ZOOM,
+    maxZoom: MAX_ZOOM - 1,
     center: L.latLng([46.879966, 11.726909]),
     timeDimension: true,
     timeDimensionControl: true,
@@ -1388,10 +1389,12 @@ export class MeteoTilesComponent implements OnInit {
       const mapCenter: L.LatLng = L.latLng(MAP_CENTER.lat, lng);
       switch (this.dataset) {
         case "lm5":
+          this.map.setMaxZoom(MAX_ZOOM - 1);
           this.map.setView(mapCenter, 5);
           break;
         case "lm2.2":
         case "iff":
+          this.map.setMaxZoom(MAX_ZOOM);
           this.map.setView(mapCenter, 6);
           break;
       }
