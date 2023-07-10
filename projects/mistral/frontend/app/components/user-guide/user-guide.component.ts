2099be1bd1e19f3ff68cb5b6480a6d31d3169f9e
Elena Saluzzi <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-07-10 13:39:57 +0200

    task #408: added responsive user guide page from mistralportal

--- projects/mistral/frontend/app/components/user-guide/user-guide.component.ts
+++ projects/mistral/frontend/app/components/user-guide/user-guide.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component,  EventEmitter, Output} from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 @Component({
     selector: "app-user-guide",
     templateUrl: "./user-guide.component.html",
     styleUrls: ["./user-guide.component.scss"]
 })
 export class UserGuideComponent{
-    constructor() { }
-    ngOnInit(): void {
-      // Add some code logic here
+    public isCollapsed = true;
+    
+    @Output() onCollapseChange: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
+    constructor(
+      private viewportScroller: ViewportScroller
+    ) { }
+
+
+
+    public onClickScroll(elementId: string): void { 
+      this.viewportScroller.scrollToAnchor(elementId);
+  
     }
-    // Or, add your logic and functionality in new functions
+    changeCollapse() {
+      this.isCollapsed = !this.isCollapsed;
+      this.onCollapseChange.emit(this.isCollapsed);
+    }
+    ngOnInit(): void {
+      
+    }
+   
+  
 }
