ff248bbff8112f4325967b561415df4637d3a9f0
Giuseppe Trotta <g.trotta@cineca.it>
2023-12-14 23:19:15 +0100

    apply rapydo pre-commit

--- projects/mistral/frontend/app/components/user-guide/user-guide.component.ts
+++ projects/mistral/frontend/app/components/user-guide/user-guide.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 @Component({
-    selector: "app-user-guide",
-    templateUrl: "./user-guide.component.html",
-    styleUrls: ["./user-guide.component.scss"]
+  selector: "app-user-guide",
+  templateUrl: "./user-guide.component.html",
+  styleUrls: ["./user-guide.component.scss"],
 })
-export class UserGuideComponent{
-    constructor() { }
-    ngOnInit(): void {
-      // Add some code logic here
-    }
-    // Or, add your logic and functionality in new functions
+export class UserGuideComponent {
+  constructor() {}
+  ngOnInit(): void {
+    // Add some code logic here
+  }
+  // Or, add your logic and functionality in new functions
 }
