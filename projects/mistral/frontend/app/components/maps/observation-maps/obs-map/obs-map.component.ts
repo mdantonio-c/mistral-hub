136853a5a39b51875b0be608767be1a77f6e083f
Mattia Carello <mcarello@cineca.it>
2024-07-22 09:58:12 +0200

    first sets

--- projects/mistral/frontend/app/components/maps/observation-maps/obs-map/obs-map.component.ts
+++ projects/mistral/frontend/app/components/maps/observation-maps/obs-map/obs-map.component.ts
@@ -665,4 +665,183 @@ export class ObsMapComponent {
     buttonItem.addTo(map);
     return buttonItem;
   }
+  setTempColor(val, rel) {
+    // code B12101
+    let color;
+    if (rel == 0) {
+      return "undefined";
+    } else {
+      if (val >= 319.15) {
+        color = "#ff9900";
+      } else if (val >= 317.15 && val < 319.15) {
+        color = "#ffcc00";
+      } else if (val >= 315.15 && val < 317.15) {
+        color = "#7200ff";
+      } else if (val >= 313.15 && val < 315.15) {
+        color = "#bf00ff";
+      } else if (val >= 311.15 && val < 313.15) {
+        color = "#ff00ff";
+      } else if (val >= 309.15 && val < 311.15) {
+        color = "#cc00cc";
+      } else if (val >= 307.15 && val < 309.15) {
+        color = "#990099";
+      } else if (val >= 305.15 && val < 307.15) {
+        color = "#660066";
+      } else if (val >= 303.15 && val < 305.15) {
+        color = "#660000";
+      } else if (val >= 301.15 && val < 303.15) {
+        color = "#990000";
+      } else if (val >= 299.15 && val < 301.15) {
+        color = "#cc0000";
+      } else if (val >= 297.15 && val < 299.15) {
+        color = "#ff0000";
+      } else if (val >= 295.15 && val < 297.15) {
+        color = "#ff6600";
+      } else if (val >= 293.15 && val < 295.15) {
+        color = "#ff9900";
+      } else if (val >= 291.15 && val < 293.15) {
+        color = "#ffcc00";
+      } else if (val >= 289.15 && val < 291.16) {
+        color = "#ffff00";
+      } else if (val >= 287.15 && val < 289.15) {
+        color = "#cce500";
+      } else if (val >= 285.15 && val < 287.15) {
+        color = "#7fcc00";
+      } else if (val >= 283.15 && val < 285.15) {
+        color = "#00b200";
+      } else if (val >= 281.15 && val < 283.15) {
+        color = "#00cc7f";
+      } else if (val >= 279.15 && val < 281.15) {
+        color = "#00e5cc";
+      } else if (val >= 277.15 && val < 279.15) {
+        color = "#00ffff";
+      } else if (val >= 275.15 && val < 277.15) {
+        color = "#00bfff";
+      } else if (val >= 273.15 && val < 275.15) {
+        color = "#008cff";
+      } else if (val >= 271.15 && val < 273.15) {
+        color = "#0059ff";
+      } else if (val >= 269.15 && val < 271.15) {
+        color = "#0000ff";
+      } else if (val >= 267.15 && val < 269.15) {
+        color = "#7200ff";
+      } else if (val >= 265.15 && val < 267.15) {
+        color = "#bf00ff";
+      } else if (val >= 263.15 && val < 265.15) {
+        color = "#ff00ff";
+      } else if (val >= 261.15 && val < 263.15) {
+        color = "#cc00cc";
+      } else if (val >= 259.15 && val < 261.15) {
+        color = "#990099";
+      } else if (val >= 257.15 && val < 259.15) {
+        color = "#660066";
+      } else if (val >= 255.15 && val < 257.15) {
+        color = "#660000";
+      } else if (val >= 253.15 && val < 255.15) {
+        color = "#990000";
+      } else if (val >= 251.15 && val < 253.15) {
+        color = "#cc0000";
+      } else if (val >= 249.15 && val < 251.15) {
+        color = "#ff0000";
+      } else if (val >= 247.15 && val < 249.15) {
+        color = "#ff6600";
+      } else if (val >= 245.15 && val < 247.15) {
+        color = "#ff9900";
+      } else if (val >= 243.15 && val < 245.15) {
+        color = "#ffcc00";
+      }
+      return color;
+    }
+  }
+  setPrecColor(val, rel) {
+    let color;
+    if (rel == 0) {
+      return "undefined";
+    } else {
+      if (val >= 300) {
+        color = "#4897D9";
+      } else if (val >= 200 && val < 300) {
+        color = "#A2A4D6";
+      } else if (val >= 100 && val < 200) {
+        color = "#B887C0";
+      } else if (val >= 75 && val < 100) {
+        color = "#D6A1CC";
+      } else if (val >= 50 && val < 75) {
+        color = "#E7BDDA";
+      } else if (val >= 40 && val < 50) {
+        color = "#E57D9A";
+      } else if (val >= 30 && val < 40) {
+        color = "#DA4C4D";
+      } else if (val >= 25 && val < 30) {
+        color = "#EE5A5C";
+      } else if (val >= 20 && val < 25) {
+        color = "#F6A15C";
+      } else if (val >= 15 && val < 20) {
+        color = "#FCD48E";
+      } else if (val >= 10 && val < 15) {
+        color = "#FFE073";
+      } else if (val >= 8 && val < 10) {
+        color = "#FDFD81";
+      } else if (val >= 6 && val < 8) {
+        color = "#FFFFC6";
+      } else if (val >= 5 && val < 6) {
+        color = "#F2F2A0";
+      } else if (val >= 4 && val < 5) {
+        color = "#D2EBA3";
+      } else if (val >= 3 && val < 4) {
+        color = "#C2E5D7";
+      } else if (val >= 2 && val < 3) {
+        color = "#C7E7EF";
+      } else if (val >= 0 && val < 2) {
+        color = "#CFEAF6";
+      }
+      return color;
+    }
+  }
+  setRhColor(val, rel) {
+    let color;
+    if (rel == 0) {
+      return "undefined";
+    } else {
+      if (val >= 100) {
+        color = "#1000FD";
+      } else if (val >= 80 && val < 100) {
+        color = "#21FEFF";
+      } else if (val >= 60 && val < 80) {
+        color = "#19FF24";
+      } else if (val >= 40 && val < 60) {
+        color = "#FEFF27";
+      } else if (val >= 20 && val < 40) {
+        color = "#FE8A12";
+      } else if (val >= 0 && val < 20) {
+        color = "#FD1506";
+      }
+      return color;
+    }
+  }
+  setWindColor(val, rel) {
+    let color;
+    if (rel == 0) {
+      return "undefined";
+    } else {
+      if (val >= 70) {
+        color = "#9600FE";
+      } else if (val >= 50 && val < 70) {
+        color = "#EE82EE";
+      } else if (val >= 30 && val < 50) {
+        color = "#FF0000";
+      } else if (val >= 20 && val < 30) {
+        color = "#FFFF00";
+      } else if (val >= 10 && val < 20) {
+        color = "#FFA600";
+      } else if (val >= 5 && val < 10) {
+        color = "#0000FE";
+      } else if (val >= 2 && val < 5) {
+        color = "#54878C";
+      } else if (val >= 0 && val < 2) {
+        color = "#457D00";
+      }
+      return color;
+    }
+  }
 }
