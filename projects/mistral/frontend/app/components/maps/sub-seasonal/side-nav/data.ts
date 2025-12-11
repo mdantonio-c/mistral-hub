9043435d58848d6627e1fdbe794d2d1ea6608948
Mattia Carello <mcarello@cineca.it>
2025-12-11 16:57:28 +0100

    update wms url for subseasonal

--- projects/mistral/frontend/app/components/maps/sub-seasonal/side-nav/data.ts
+++ projects/mistral/frontend/app/components/maps/sub-seasonal/side-nav/data.ts
@@ -17,17 +17,17 @@ export const MEDITA_BOUNDS = {
 
 export const Layers = {
   t2m: {
-    terzile_1: "meteohub:t2m_terzile_1",
-    terzile_2: "meteohub:t2m_terzile_2",
-    terzile_3: "meteohub:t2m_terzile_3",
-    quintile_1: "meteohub:t2m_quintile_1",
-    quintile_5: "meteohub:t2m_quintile_5",
+    terzile_1: "meteohub:sub-seasonal-t2m-terzile_1",
+    terzile_2: "meteohub:sub-seasonal-t2m-terzile_2",
+    terzile_3: "meteohub:sub-seasonal-t2m-terzile_3",
+    quintile_1: "meteohub:sub-seasonal-t2m-quintile_1",
+    quintile_5: "meteohub:sub-seasonal-t2m-quintile_5",
   },
   tp: {
-    terzile_1: "meteohub:tp_terzile_1",
-    terzile_2: "meteohub:tp_terzile_2",
-    terzile_3: "meteohub:tp_terzile_3",
-    quintile_1: "meteohub:tp_quintile_1",
-    quintile_5: "meteohub:tp_quintile_5",
+    terzile_1: "meteohub:sub-seasonal-tp-terzile_1",
+    terzile_2: "meteohub:sub-seasonal-tp-terzile_2",
+    terzile_3: "meteohub:sub-seasonal-tp-terzile_3",
+    quintile_1: "meteohub:sub-seasonal-tp-quintile_1",
+    quintile_5: "meteohub:sub-seasonal-tp-quintile_5",
   },
 };
