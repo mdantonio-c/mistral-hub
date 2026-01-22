caf30a44b38f4f3f52ece6b4991f5c04c9d62d72
Mattia Carello <mcarello@cineca.it>
2026-01-22 11:59:37 +0100

    add normal stacked vertical bar chart

--- projects/mistral/frontend/app/components/maps/sub-seasonal/province-expandend-report/province-expanded-report.component.ts
+++ projects/mistral/frontend/app/components/maps/sub-seasonal/province-expandend-report/province-expanded-report.component.ts
@@ -17,22 +17,35 @@ export class ProvinceExpandedReportComponent {
   @Input() lang!: string;
   @Input() prov!: string;
   selectedMetric: "Temperature" | "Total Precipitation" = "Temperature";
-  yLabel;
+  yLabel = "Probability (%)";
   province;
+  multi;
+  processedData;
 
+  colorSchemeTemp = {
+    domain: ["#003366", "#99CCFF", "#FFFFFF", "#FF9999", "#8B0000"],
+  };
+
+  colorSchemePrec = {
+    domain: ["#E65100", "#FFB300", "#FFFFFF", "#66BB6A", "#1B5E20"],
+  };
+  colorScheme = this.colorSchemeTemp;
   public selectMetric(metric: "Temperature" | "Total Precipitation") {
     this.selectedMetric = metric;
-    this.yLabel =
-      metric === "Total Precipitation"
-        ? "Precipitation (mm)"
-        : "Temperature (°C)";
+    if (metric == "Temperature") {
+      this.multi = this.processedData.temp;
+      this.colorScheme = { ...this.colorSchemeTemp };
+    } else {
+      this.multi = this.processedData.prec;
+      this.colorScheme = { ...this.colorSchemePrec };
+    }
     this.cdr.detectChanges();
   }
 
   public beforeOpen(layerId: string) {
     this.cdr.detectChanges();
     this.changeProvinceName(this.prov);
-    this.zone.run(() => this.cdr.markForCheck());
+    this.loadReport(layerId);
   }
   changeProvinceName(prov: string) {
     this.province = prov;
@@ -46,5 +59,83 @@ export class ProvinceExpandedReportComponent {
     if (prov == "Sud Sardegna") this.province = "Carbonia";
     if (prov == "Verbano-Cusio-Ossola") this.province = "Verbania";
     if (prov == "Reggio di Calabria") this.province = "Reggio Calabria";
+  }
+  private loadReport(layerId: string) {
+    this.loadProvinceData(layerId);
+  }
+  private async loadProvinceData(layerId: string) {
+    try {
+      const response = await fetch(
+        `./app/custom/assets/images/json_out_subseasonal/${
+          this.prov
+        }.json?t=${new Date().getTime()}`,
+        { cache: "no-store" },
+      );
+      const data = await response.json();
+      this.processedData = this.prepareDataForChart(data);
+      this.zone.run(() => {
+        (this.selectedMetric as any) = layerId;
+        if (layerId == "Temperature") {
+          this.multi = this.processedData.temp;
+          this.colorScheme = { ...this.colorSchemeTemp };
+        } else {
+          this.colorScheme = { ...this.colorSchemePrec };
+          this.multi = this.processedData.prec;
+        }
+        this.cdr.markForCheck();
+      });
+    } catch (error) {
+      console.error("Errore caricamento JSON provincia:", error);
+    }
+  }
+  private prepareDataForChart(data) {
+    const dataTempSet = data.variabili.Temperatura;
+    const dataPrecSet = data.variabili.Precipitazione;
+    const dataTemp = [];
+    const dataPrec = [];
+
+    dataTempSet.forEach((el) => {
+      dataTemp.push({
+        name: el.settimana,
+        series: [
+          {
+            name: "Colder",
+            value: el.quintili[0].value,
+          },
+          {
+            name: "Below average",
+            value: el.quintili[1].value,
+          },
+          { name: "Average", value: el.quintili[2].value },
+          { name: "Above average", value: el.quintili[3].value },
+          { name: "Warmer", value: el.quintili[4].value },
+        ],
+        soglie: el.soglie,
+      });
+    });
+    dataPrecSet.forEach((el) => {
+      dataPrec.push({
+        name: el.settimana,
+        series: [
+          {
+            name: "Drier",
+            value: el.quintili[0].value,
+          },
+          {
+            name: "Below average",
+            value: el.quintili[1].value,
+          },
+          { name: "Average", value: el.quintili[2].value },
+          { name: "Above average", value: el.quintili[3].value },
+          { name: "Wetter", value: el.quintili[4].value },
+        ],
+        soglie: el.soglie,
+      });
+    });
+
+    return { temp: dataTemp, prec: dataPrec };
+  }
+  onSelect(event) {
+    console.log(event);
   }
 }
