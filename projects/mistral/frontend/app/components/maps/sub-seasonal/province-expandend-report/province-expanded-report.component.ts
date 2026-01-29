24854e678c6af3efc758bde78748e61f82ba235c
Mattia Carello <mcarello@cineca.it>
2026-01-29 16:00:32 +0100

    fixed 1 decimal digit for tooltip #773

--- projects/mistral/frontend/app/components/maps/sub-seasonal/province-expandend-report/province-expanded-report.component.ts
+++ projects/mistral/frontend/app/components/maps/sub-seasonal/province-expandend-report/province-expanded-report.component.ts
@@ -137,8 +137,12 @@ export class ProvinceExpandedReportComponent implements AfterViewInit {
     if (!indices) return [""];
 
     const unit = layerId === "Temperature" ? "°C" : "mm";
-    const minThreshold = weekData.soglie[indices[1]].value;
-    const maxThreshold = weekData.soglie[indices[0]].value;
+    const minThreshold = Number(weekData.soglie[indices[1]].value)
+      .toFixed(1)
+      .toString();
+    const maxThreshold = Number(weekData.soglie[indices[0]].value)
+      .toFixed(1)
+      .toString();
 
     return [`${minThreshold}${unit}`, `${maxThreshold}${unit}`];
   }
